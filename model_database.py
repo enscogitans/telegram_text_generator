d5d493883e9f1bcca94e11755f6a561813f0242a
enscogitans <andrew-199@ya.ru>
2018-05-13 01:00:23 +0300

    - Changed the name of the class - Added function which creates the database - Added function which adds a record to the database

--- model_database.py
+++ model_database.py
@@ -1,6 +1,7 @@
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -16,10 +17,11 @@ class Record(Base):
     chat_id = Column(String, primary_key=True)
     token_0 = Column(String, primary_key=True)
     token_1 = Column(String, primary_key=True)
+    num = Column(Integer, default=1)
 
     def __repr__(self):
-        return "<Model(chat_id={}, token_0={}, token_1={})>".format(
-            self.chat_id, self.token_0, self.token_1)
+        return "<Record(chat_id={}, token_0={}, token_1={}, num={})>".format(
+            self.chat_id, self.token_0, self.token_1, self.num)
 
 
 # Подключение к нашей базе данных
@@ -36,12 +38,27 @@ def create_database():
 # Функция добавления записи в таблицу. Возвращает True, если запись была создана,
 # False, если такая запись уже существует
 def try_add_record(chat_id, token_0, token_1):
-    session.add(Record(chat_id=chat_id,
-                       token_0=token_0,
-                       token_1=token_1))
+    session.add(Record(chat_id=chat_id, token_0=token_0, token_1=token_1))
+
     try:
         session.commit()
         return True
     except IntegrityError:  # Если запись существует, отмена коммита
         session.rollback()
         return False
+
+
+# Функция изменения в записи поля num. Возвращает True, если запись была изменена,
+# False, если записи не существует
+def try_set_num(chat_id, token_0, token_1, new_num):
+    try:
+        record = session.query(Record).filter_by(chat_id=chat_id,
+                                                 token_0=token_0,
+                                                 token_1=token_1).one()
+    except NoResultFound:   # Если такой записи нет, вернуть False
+        return False
+
+    record.num = new_num
+
+    session.commit()
+    return True
