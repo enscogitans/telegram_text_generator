edf0f2cf2f49804e37129311be100fd3697ce2b3
enscogitans <andrew-199@ya.ru>
2018-05-13 02:12:41 +0300

    - Edited token_generator. Now it takes the only line instead of list of lines. is_lower is False by default - Edited initialise_model. It was renamed to initialise_changes. Now it also saves a number of meeting each bigram to the model instead of frequency - Deleted save_model - Some changes in update_model

--- train_model.py
+++ train_model.py
@@ -1,5 +1,4 @@
 import re
-from fractions import Fraction
 from collections import defaultdict
 
 
@@ -10,10 +9,9 @@ encode_type = 'utf-8'
 
 # Обрабатывает строки, возвращая отдельные слова, согласованные
 # с алфавитом, и, при необходимости, в нижнем регистре
-def token_generator(lines, is_lower):
-    for line in lines:
-        for token in alphabet.findall(line):
-            yield token.lower() if is_lower else token
+def token_generator(text, is_lower=False):
+    for token in alphabet.findall(text):
+        yield token.lower() if is_lower else token
 
 
 # По словам (токенам) возвращает биграммы
@@ -48,34 +46,22 @@ def count_words_and_pairs(bigrams):
     return word_freq, pair_freq
 
 
-# Инициализация нашей модели. По ключу хранится лист слов,
-# которые могут идти после него с указаним вероятности (в виде рац. дроби)
-def initialise_model(word_freq, pair_freq):
+# Инициализация изменений нашей модели. По ключу хранится лист слов,
+# которые могут идти после него с указаним количества встречаний
+def initialise_changes(word_freq, pair_freq):
     model = defaultdict(list)
     for (t0, t1), freq in pair_freq.items():
-        model[t0].append((t1, Fraction(freq, word_freq[t0])))
+        model[t0].append((t1, freq))
     return model
 
 
-# Сохранение модели в файл model.txt в указанной папке
-def save_model(model, model_path):
-    with open('{}\\model.txt'.format(model_path), 'w',
-              encoding=encode_type) as model_file:
-        for key, lst in model.items():
-            model_file.write(key)
-            for (word, freq) in lst:
-                model_file.write(' {} {}'.format(word, freq))
-            model_file.write('\n')
-
-
-def update_model(lines):
+def update_model(text):
     # Инициализация генератора биграмм
-    tokens = token_generator(lines)
+    tokens = token_generator(text)
     bigrams = bigram_generator(tokens)
 
     # Подсчёт количества слов и пар слов соответственно
     word_freq, pair_freq = count_words_and_pairs(bigrams)
 
-    # Инициализация и сохранение нашей модели
-    model = initialise_model(word_freq, pair_freq)
-    save_model(model, args.model)
+    # Инициализация нашей модели
+    model = initialise_changes(word_freq, pair_freq)
